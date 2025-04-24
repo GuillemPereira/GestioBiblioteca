81d4896087d5d3e906eebbb863da0dc5fb5b52b9
Alan Garcia <alan.garcia@insbaixcamp.cat>
2025-04-24 17:39:05 +0200

    Daily_update_alan

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,3 +1,12 @@
 public class Biblioteca{
-    
+    private ArrayLlibres llibres;
+
+    public Biblioteca() {
+        this.llibres = new ArrayLlibres(10);
+        }
+
+    public void afegirLlibre(ArrayLlibres llibres){
+        
+    }
+
 }
